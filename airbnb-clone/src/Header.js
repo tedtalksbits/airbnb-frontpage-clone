9c22f109fbc31d23a44d48e3eef49ef96cd6f092
Tedane Blake <tedaneblake@MacBook-Pro.fios-router.home>
2020-10-21 22:59:33 -0400

    added and styled header component

--- airbnb-clone/src/Header.js
+++ airbnb-clone/src/Header.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import { Avatar, Button, IconButton } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import LanguageIcon from '@material-ui/icons/Language';
 
 function Header() {
     return (
@@ -7,12 +11,24 @@ function Header() {
         
                 <img 
                     className="header__icon" 
-                    src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png" alt=""
+                    src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png" alt="air bnb logo"
                 />
 
                 <div className="header__center">
                     <input type="text"/>
                     <SearchIcon />
+                </div>
+
+                <div className="header__right">
+                    <Button>
+                        <div className="nowrap">Become a host</div>
+                    </Button>
+                    <Button >
+                        <LanguageIcon />
+                        <ExpandMoreIcon />
+                    </Button>
+                    <Avatar />
+
                 </div>
         </div>
     )
